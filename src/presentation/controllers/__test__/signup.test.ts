f88430822043a13623b0998109420d799bc9c147
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-26 22:12:24 -0300

    test: ensure SigUpController returns 400 if name field is not provided

---
+++ src/presentation/controllers/__test__/signup.test.ts
@@ -0,0 +1,18 @@
+import { SignUpcontroller } from '@src/presentation/controllers/signup'
+
+describe('SignUp Controller', ()=> {
+  it('Should return 400 if name not provided', async () => {
+    const sut = new SignUpcontroller()
+
+    const httpRequest = {
+      body: {
+        email: 'any-email@mail.com',
+        password: 'anny_password',
+        passwordConfirm: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
