becbca545bce9029569ba8484eff2c695b798cda
Jean Fraga <jeanfragoli395@gmail.com>
2022-03-01 16:42:56 -0300

    refactor: make sistem under test factory

--- src/infra/criptography/bcrypt-adapter.test.ts
+++ src/infra/criptography/bcrypt-adapter.test.ts
@@ -1,26 +1,31 @@
 import bcrypt, { hash } from 'bcrypt'
 import { setDefaultResultOrder } from 'dns'
 import { BcryptAdapter } from './bcrypt-adapter'
+
 jest.mock('bcrypt', () => ({
   async hash(): Promise<String> {
     return new Promise(resolve => resolve('hashed-value'))
   }
 }))
 
-
+const salt = 12
+const makeSut = (salt:number): BcryptAdapter => {
+   return new BcryptAdapter(salt)
+}
 describe('Bcrypt Adapter', () => {
-  const salt = 12
+  
+
 
   it('Should call bcrypt with correct values', async ()=> {
 
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash') 
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   it('sould return a hashed value', async () => {
-    const sut = new BcryptAdapter(salt) 
+    const sut = makeSut(salt)
     const hashed = await sut.encrypt('any-value')
     expect(hashed).toBe('hashed-value')
   })
