004f509e7591034570079a56bc51a55de6809399
Jean Fraga <jeanfragoli395@gmail.com>
2022-03-01 15:54:30 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

--- src/data/protocols/add-account-repository.ts
+++ src/data/protocols/add-account-repository.ts
@@ -3,6 +3,7 @@ import { AccountModel } from '@src/domain/models/account'
 
 
 export class AddAccountRepository implements AddAccount  {
-  public async add (accountData: AddAccountModel): Promise<AccountModel>
-  
+  public async add (accountData: AddAccountModel): Promise<AccountModel> {
+    return new Promise(resolve => resolve(null))
+  }
 }
