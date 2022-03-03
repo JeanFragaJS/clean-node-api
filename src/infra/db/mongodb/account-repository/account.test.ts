7eaa9123d6edbc8c6b394c385107f86946a696b8
Jean Fraga <jeanfragoli395@gmail.com>
2022-03-03 18:15:46 -0300

    refactor: add a makeSut and add deleteMany between tests

--- src/infra/db/mongodb/account-repository/account.test.ts
+++ src/infra/db/mongodb/account-repository/account.test.ts
@@ -1,15 +1,27 @@
 import { MongoHelper } from '../helpers/mongo-helpers'
 import { AccountMongoRepository } from './account'
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository
+}
+
 describe('Account Mondo Repository', ()=> {
+  
   beforeAll( async ()=> {
       await MongoHelper.connect('mongodb://127.0.0.1:27017/')
+  })
+
+  beforeEach( async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection.deleteMany({})
   })
 
   afterAll( async ()=> {
     await MongoHelper.disconnect()
   })
+
   it('Should return an account on success', async () => {
-    const sut =  new AccountMongoRepository()
+    const sut = makeSut()
     const dataFake = {
       name: 'any-name',
       email: 'any@mail',
