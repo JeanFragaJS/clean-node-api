e4f3e045a6a9d67441857101bc6837d535901f43
Jean Fraga <jeanfragoli395@gmail.com>
2022-03-06 09:53:11 -0300

    refactor: move all middlewares to index

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,13 +1,11 @@
 import { Express } from 'express'
-import { bodyParser } from '../middlewares/body-parser'
-import { contenType } from '../middlewares/content-type'
-import { cors } from '../middlewares/cors'
+import { bodyParser, cors, contenType } from '../middlewares'
+
 
 
 export default (app: Express): void => { 
   app.use(bodyParser)
   app.use(cors)
   app.use(contenType)
- 
   
 }
