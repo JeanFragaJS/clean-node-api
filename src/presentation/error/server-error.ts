c7179e60568c4e5983bffc8baa24eb0030beb0dd
Jean Fraga <jeanfragoli395@gmail.com>
2022-03-09 16:48:59 -0300

    refactor: make server error return error stack

--- src/presentation/error/server-error.ts
+++ src/presentation/error/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-  constructor() {
+  constructor(stack: string) {
     super(`Server Error, try again later`);
     this.name = 'ServerError';
+    this.stack = stack
   }
 }
