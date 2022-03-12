5e7b3d63508b622577bdc00926fa324cd3b23a47
Jean Fraga <jeanfragoli395@gmail.com>
2022-03-12 11:15:13 -0300

    feat: ensure LoginController returns 500 if EmailValidator Throws error

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError, MissingParamError } from '@src/presentation/error';
 import { Controller, HttpRequest, HttpResponse, EmailValidator} from './login-protocols';
-import { badRequest, unauthorized } from '../../helpers/http-helper'
+import { badRequest, unauthorized, serverError } from '../../helpers/http-helper'
 import { Authentication } from '@src/domain/usecases/authentication'
 
 export class LoginController implements Controller{
@@ -13,26 +13,31 @@ export class LoginController implements Controller{
   }
   public async handle  (httpRequest: HttpRequest): Promise<HttpResponse> {
 
-    // check the fields exists
-    const  fields = ['password', 'email']
-    for (const field of fields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try{
+      // check the fields exists
+      const  fields = ['password', 'email']
+      for (const field of fields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    // check the email is valid
-    const { email, password } = httpRequest.body
-    const isValidEmail = this.emailValidator.isValid(email)
-    if (!isValidEmail) {
-      return badRequest(new InvalidParamError('email'))
+      // check the email is valid
+      const { email, password } = httpRequest.body
+      const isValidEmail = this.emailValidator.isValid(email)
+      if (!isValidEmail) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      const accessToken = await this.authentication.auth(email, password)
+      if (!accessToken) {
+        return unauthorized() 
+      }
+      return new Promise(resolve => resolve(null))
+
+    } catch (error) {
+      return serverError(error)
     }
 
-    const accessToken = await this.authentication.auth(email, password)
-    if (!accessToken) {
-      return unauthorized() 
-    }
-    return new Promise(resolve => resolve(null))
-    
   }
 
 }
