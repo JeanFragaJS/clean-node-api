054f2a0b89b1f54f52dd182a3c661965ce6925e8
Jean Fraga <jeandevjs@gmail.com>
2022-03-14 20:18:47 -0300

    test: ensure EmailValidation calls EmailValidator with correct values and throw if EmailValidator Throws

---
+++ src/presentation/helpers/validators/__test__/email-validation.test.ts
@@ -0,0 +1,47 @@
+import { EmailValidator } from "@src/presentation/protocols";
+import { EmailValidation } from "../emailValidation";
+
+  const makeEmailvalidator = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+      public isValid(email: string): boolean {
+        return true;
+      }
+    }
+    return new EmailValidatorStub();
+  };
+
+  interface SutTypes {
+    emailValidatorStub: EmailValidator
+    sut: EmailValidation
+  }
+
+  const makeSut = (): SutTypes => {
+    const emailValidatorStub = makeEmailvalidator()
+    const sut = new EmailValidation('email', emailValidatorStub)
+    return {
+      sut,
+      emailValidatorStub,
+    }
+  }
+
+describe('Email Validation', () => {
+
+  
+  it('Should call the isValid method with the correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
+    const input = { email: 'any@mail.com'}
+    sut.validate(input);
+    expect(isValidSpy).toHaveBeenCalledWith('any@mail.com');
+  });
+
+  it('Should throws if isValid method throw  Error', () => {
+    const {sut, emailValidatorStub} = makeSut() 
+    jest.spyOn(emailValidatorStub, 'isValid')
+      .mockImplementationOnce(()=> {
+        throw new Error()
+      })
+    
+    expect(sut.validate).toThrow()
+  });
+} )
