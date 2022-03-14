73ebb2f8294b27749563e9ca3a215be5936367dd
Jean Fraga <jeandevjs@gmail.com>
2022-03-14 20:39:16 -0300

    test: add test to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/__test__/compare-fields.test.ts
@@ -0,0 +1,21 @@
+import { CompareFieldsValidation } from "../compareFieldsValidation";
+import {InvalidParamError} from '@src/presentation/error/invalid-param-error'
+
+describe('CompareFieldsValidation', () => {
+  
+  it('Should return 400 if passwordConfirm fail', async () => {
+    const  sut  = new CompareFieldsValidation( 'field', 'fieldToCompare')
+    const error =  sut.validate({field: 'password', fieldToCompare: 'passwordWrong'});
+
+    
+    expect(error).toEqual( new InvalidParamError('field'));
+   
+  });
+
+  it('Should not return if validation success', ()=> {
+    const sut  = new CompareFieldsValidation('field', 'fieldToCompare')
+    const error = sut.validate({field: 'any', fieldToCompare: 'any'})
+
+    expect(error).toBeFalsy()
+  })
+})
