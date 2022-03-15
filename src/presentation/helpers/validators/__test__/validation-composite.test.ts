86bc759437d1a4374ac05ead1db28ae40d4072e1
Jean Fraga <jeandevjs@gmail.com>
2022-03-15 19:14:28 -0300

    test: move sut criation to a factory

--- src/presentation/helpers/validators/__test__/validation-composite.test.ts
+++ src/presentation/helpers/validators/__test__/validation-composite.test.ts
@@ -2,19 +2,32 @@ import { MissingParamError } from "@src/presentation/error"
 import { Validation } from "../validation"
 import { ValidationComposite } from "../validationsComposite"
 
-const makeValidationStub = (): Validation => {
+const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     public validate (input: any): Error {
-      return new MissingParamError('field')
+      return null
     }
   }
   return new ValidationStub()
+}
+interface SutTypes  {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
 }
 
 describe('Validation Composite', () => {
 
   it('Should return an error if any validation fails', () => {
-    const sut = new ValidationComposite([makeValidationStub()])
+    const { sut, validationStub} = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({field: 'any-value'})
 
     expect(error).toEqual(new MissingParamError('field'))
