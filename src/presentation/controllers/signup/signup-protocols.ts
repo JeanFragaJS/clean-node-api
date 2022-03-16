7128101bbf4341f35288cfd7cdd84b8fc22bf877
Jean Fraga <jeandevjs@gmail.com>
2022-03-15 23:03:55 -0300

    refactor: remove unusable code

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,5 +1,4 @@
 export * from '@src/presentation/protocols'
-export * from '../../protocols/email-validator'
 export * from '@src/domain/models/account'
 export * from '@src/domain/usecases/add-account'
 export * from '../../helpers/validators/validation'
