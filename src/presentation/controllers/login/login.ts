6289b2e20bcd6ee8df79d8eeb46028f9f239e50e
Jean Fraga <jeandevjs@gmail.com>
2022-03-15 23:27:55 -0300

    feat: ensure LoginController call validation with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,34 +1,24 @@
 import { InvalidParamError, MissingParamError } from '@src/presentation/error';
-import { Controller, HttpRequest, HttpResponse, EmailValidator} from './login-protocols';
+import { Controller, HttpRequest, HttpResponse, Validation} from './login-protocols';
 import { badRequest, unauthorized, serverError, ok} from '../../helpers/http-helper'
 import { Authentication } from '@src/domain/usecases/authentication'
 
 export class LoginController implements Controller{
-  private readonly emailValidator: EmailValidator
   private readonly authentication: Authentication
+  private readonly validation: Validation
 
-  constructor (emailValidator: EmailValidator, authentication: Authentication) {
-    this.emailValidator = emailValidator
+  constructor ( authentication: Authentication, validation: Validation) {
     this.authentication = authentication
+    this.validation = validation
   }
   public async handle  (httpRequest: HttpRequest): Promise<HttpResponse> {
 
     try{
-      // check the fields exists
-      const  fields = ['password', 'email']
-      for (const field of fields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
-      }
-      // check the email is valid
-      const { email, password } = httpRequest.body
-      const isValidEmail = this.emailValidator.isValid(email)
-      if (!isValidEmail) {
-        return badRequest(new InvalidParamError('email'))
-      }
 
+      const error = this.validation.validate(httpRequest.body)
+      
       // chek if the user is authorized
+      const { email, password } = httpRequest.body
       const accessToken = await this.authentication.auth(email, password)
       if (!accessToken) {
         return unauthorized() 
