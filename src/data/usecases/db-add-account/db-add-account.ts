f3c9be959bc06eef4e53aa6856c9d5d5e5efdc14
Jean Fraga <jeandevjs@gmail.com>
2022-06-12 10:14:24 -0300

    refactor:  refactor style

--- src/data/usecases/db-add-account/db-add-account.ts
+++ src/data/usecases/db-add-account/db-add-account.ts
@@ -8,7 +8,7 @@ import {
 } from './db-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
-  
+
   constructor(
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
@@ -17,11 +17,13 @@ export class DbAddAccount implements AddAccount {
 
   public async add(accountData: AddAccountModel): Promise<AccountModel> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+
     if (!account ) {
       const hashedPassword = await this.hasher.hash(accountData.password);
       const newAccount = await this.addAccountRepository.add( Object.assign({}, accountData, { password: hashedPassword }));
       return newAccount;
     }
+
     return null;
     //Não vamos colocar o try-catch aqui  justamente para se caso ocorra um erro
     //ser lançado a diante
